f91faf0f149d0362f6920656b6a68e93e371b76d
Tamsanqa743 <79663654+Tamsanqa743@users.noreply.github.com>
2021-10-21 23:42:03 +0200

    Add files via upload
    Added database

---
+++ HotelGroupSystem/Business/RoomController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Collections.ObjectModel;
+using HotelGroupSystem.Data;
+
+namespace HotelGroupSystem.Business
+{
+    class RoomController
+    {
+        #region Data Members
+        RoomDB roomDB;
+        Collection<Room> rooms;
+        #endregion
+
+        #region Properties
+        public Collection<Room> AllRooms
+        {
+            get
+            {
+                return rooms;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public RoomController()
+        {
+            roomDB = new RoomDB();
+            rooms = roomDB.AllRooms;
+        }
+
+        #endregion
+
+        #region Database Communication.
+        public void DataMaintenance(Room aRoom, DB.DBOperation operation)
+        {
+            int index = 0;
+            //perform a given database operation to the dataset in meory; 
+            roomDB.DataSetChange(aRoom, operation);
+            //perform operations on the collection
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    //*** Add the employee to the Collection
+                    rooms.Add(aRoom);
+                    break;
+                case DB.DBOperation.Update:
+                    index = FindIndex(aRoom);
+                    rooms[index] = aRoom;  // replace room at this index with the updated room
+                    break;
+                case DB.DBOperation.Delete:
+                    index = FindIndex(aRoom);  // find the index of the specific room in collection
+                    rooms.RemoveAt(index);  // remove that room from the collection
+                    break;
+
+            }
+        }
+
+        //***Commit the changes to the database
+        public bool FinalizeChanges(Room aRoom)
+        {
+            //***call the roomDB method that will commit the changes to the database
+            return roomDB.UpdateDataSource(aRoom);
+        }
+        #endregion
+
+        #region Searching through a collection
+
+        //This method receives a room number as a parameter; finds the booking object in the collection of guests and then returns this object
+        public Room Find(int roomNum)
+        {
+            int index = 0;
+            //check if it is the first guest
+            bool found = (rooms[index].RoomNumber == roomNum);
+            int count = rooms.Count;
+            while (!(found) && (index < rooms.Count - 1))
+            {
+                index = index + 1;
+                found = (rooms[index].RoomNumber == roomNum);   // this will be TRUE if found
+            }
+            return rooms[index];
+        }
+
+        public int FindIndex(Room aRoom)
+        {
+            int counter = 0;
+            bool found = false;
+            found = (aRoom.RoomNumber == rooms[counter].RoomNumber);   //using a Boolean Expression to initialise found
+            while (!(found) & counter < rooms.Count - 1)
+            {
+                counter += 1;
+                found = (aRoom.RoomNumber == rooms[counter].RoomNumber);
+            }
+            if (found)
+            {
+                return counter;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ HotelGroupSystem/Data/RoomDB.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelGroupSystem.Business;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+
+namespace HotelGroupSystem.Data
+{
+    class RoomDB: DB
+    {
+        #region Data Members
+        private string table3 = "Room";
+        private string sqlLocal3 = "SELECT * FROM Room";
+        private Collection<Room> rooms;
+        #endregion
+
+        #region Property Method: Collection
+        public Collection<Room> AllRooms
+        {
+            get
+            {
+                return rooms;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public RoomDB() : base()
+        {
+            //instantiate guest collection
+            rooms = new Collection<Room>();
+            //fill data set
+            FillDataSet(sqlLocal3, table3);
+            Add2Collection(table3);
+        }
+        #endregion
+
+        #region Utility Methods
+        public DataSet GetDataSet()
+        {
+            return dsMain;
+        }
+
+        private void Add2Collection(string table)
+        {
+            DataRow myRow = null;
+            Room room;
+
+            foreach (DataRow myRow_loopVariable in dsMain.Tables[table].Rows)
+            {
+                myRow = myRow_loopVariable;
+                if (!(myRow.RowState == DataRowState.Deleted))
+                {
+                    room= new Room();
+
+                    //Obtain each Room attribute from the specific field in the row in the table
+                    room.RoomState = (Room.RoomStatus)(myRow["RoomState"]);
+                    room.SeasonalRate = (Room.RateSeason)(myRow["Rate"]);
+                    //add booking to bookings collection
+                    rooms.Add(room);
+
+                }
+            }
+        }
+
+        private void FillRow(DataRow aRow, Room aRoom, DB.DBOperation operation)
+        {
+
+            if (operation == DB.DBOperation.Add)
+            {
+                //aRow["GuestID"] = aGuest.getGuestID;  //NOTE square brackets to indicate index of collections of fields in row.
+            }
+
+        }
+
+       private int FindRow(Room aRoom, string table)
+        {
+            int rowIndex = 0;
+            DataRow myRow;
+            int returnValue = -1;
+            foreach (DataRow myRow_loopVariable in dsMain.Tables[table].Rows)
+            {
+                myRow = myRow_loopVariable;
+                //Ignore rows marked as deleted in dataset
+                if (!(myRow.RowState == DataRowState.Deleted))
+                {
+                    //check if guest id equal to what we are looking for
+                    if (aRoom.RoomNumber == Convert.ToInt32(dsMain.Tables[table].Rows[rowIndex]["RoomNumber"]))
+                    {
+                        returnValue = rowIndex;
+                    }
+                }
+                rowIndex += 1;
+            }
+            return returnValue;
+        }
+
+        #endregion
+
+        #region Build Parameters, Create Commands & Update database
+        /*private void Build_INSERT_Parameters(Room aRoom)
+        {
+            //Create Parameters to communicate with SQL INSERT...add the input parameter and set its properties.
+            SqlParameter param = default(SqlParameter);
+            param = new SqlParameter("@RoomNumber", SqlDbType.SmallInt, 5, "RoomNumber");
+            daMain.InsertCommand.Parameters.Add(param);//Add the parameter to the Parameters collection.
+
+            param = new SqlParameter("@GuestID", SqlDbType.NVarChar, 10, "GuestID");
+            daMain.InsertCommand.Parameters.Add(param);
+
+
+            param = new SqlParameter("@Age", SqlDbType.SmallInt, 100, "Age");
+            daMain.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@Phone", SqlDbType.NVarChar, 15, "Phone");
+            daMain.InsertCommand.Parameters.Add(param);
+
+            param = new SqlParameter("@Address", SqlDbType.NVarChar, 10, "Address");
+            daMain.InsertCommand.Parameters.Add(param);
+
+        }*/
+
+
+        private void Build_UPDATE_Parameters(Room aRoom)
+        {
+            //---Create Parameters to communicate with SQL UPDATE
+            SqlParameter param = default(SqlParameter);
+
+
+            //create and add update room status parameter
+            param = new SqlParameter("@RoomStatus", SqlDbType.SmallInt, 5, "RoomStatus");
+            param.SourceVersion = DataRowVersion.Current;
+            daMain.UpdateCommand.Parameters.Add(param);
+
+            //create and add update room rate paramete
+            param = new SqlParameter("@RoomRate", SqlDbType.SmallMoney, 15, "RoomRate");
+            param.SourceVersion = DataRowVersion.Current;
+            daMain.UpdateCommand.Parameters.Add(param);
+
+            //testing the booking reference of record that needs to change with the original guestID of the record
+            param = new SqlParameter("@Original_RoomNumber", SqlDbType.NVarChar, 15, "RoomNumber");
+            param.SourceVersion = DataRowVersion.Original;
+            daMain.UpdateCommand.Parameters.Add(param);
+
+        }
+
+ /*       private void Build_DELETE_Parameters()
+        {
+            //Create Parameters to communicate with SQL DELETE
+            SqlParameter param;
+            param = new SqlParameter("@BookingReference", SqlDbType.NVarChar, 15, "BookingReference");
+            param.SourceVersion = DataRowVersion.Original;
+            daMain.DeleteCommand.Parameters.Add(param);
+        }
+ */
+ /*       private void Create_INSERT_Command(Guest aGuest)
+        {
+            //Command used to insert values into the Bookings table..
+
+            daMain.InsertCommand = new SqlCommand("INSERT into Bookings (GuestID, BookingReference, RoomsBooked, RoomRate, TotalDue, CheckInDate, CheckOutDate) VALUES (@GuestID, @BookingReference, @RoomsBooked, @RoomRate, @TotalDue, @CheckInDate,CheckOutDate)", cnMain);
+            Build_INSERT_Parameters(aGuest);
+        }*/
+
+        private void Create_UPDATE_Command(Room aRoom)
+        {
+            //Command that must be used to insert values into bookings table
+            //The GuestID and BookingReference cannot be changed
+
+            daMain.UpdateCommand = new SqlCommand("UPDATE Room SET RoomRate =@RoomRate, RoomStatus =@RoomStatus " + "WHERE RoomNumber = @Original_RoomNumber", cnMain);
+            Build_UPDATE_Parameters(aRoom);
+        }
+
+ /*       private string Create_DELETE_Command(Guest aGuest)
+        {
+            string errorString = null;
+            //Command used to delete values from the Guest table
+            daMain.DeleteCommand = new SqlCommand("DELETE FROM Guest WHERE GuestID = @GuestID", cnMain);
+
+            try
+            {
+                Build_DELETE_Parameters();
+            }
+            catch (Exception errObj)
+            {
+                errorString = errObj.Message + "  " + errObj.StackTrace;
+            }
+            return errorString;
+        }*/
+
+        //update data source
+        public bool UpdateDataSource(Room aRoom)
+        {
+            bool success = true;
+            
+            Create_UPDATE_Command(aRoom);
+
+            success = UpdateDataSource(sqlLocal3, table3);
+            return success;
+        }
+
+        #endregion
+
+        #region Database Operations CRUD
+        public void DataSetChange(Room aRoom, DB.DBOperation operation)
+        {
+            DataRow aRow = null;
+            string dataTable = table3;
+
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    aRow = dsMain.Tables[dataTable].NewRow();
+                    FillRow(aRow, aRoom, operation);
+                    dsMain.Tables[dataTable].Rows.Add(aRow);
+                    //Add to the dataset
+                    break;
+                case DB.DBOperation.Update:
+                    // Find row to update
+                    aRow = dsMain.Tables[dataTable].Rows[FindRow(aRoom, dataTable)];
+                    //Fill this row for the update operation by calling the FillRow method
+                    FillRow(aRow, aRoom, operation);
+                    break;
+                case DB.DBOperation.Delete:
+                    //find row and delete it
+                    aRow = dsMain.Tables[dataTable].Rows[FindRow(aRoom, dataTable)];
+                    aRow.Delete();
+                    break;
+            }
+        }
+        #endregion
+    }
+}
